701f509e2cae13b80a07a8c4ded291112305d53a
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-08-31 12:56:07 +0100

    Make VarnishClearer use new Purger class

--- app/services/varnish_clearer.rb
+++ app/services/varnish_clearer.rb
@@ -1,38 +1,20 @@
 class VarnishClearer
   def initialize(logger)
     @logger = logger
+    @purger = Purger.new(logger)
   end
 
   def clear_for(base_path)
     GovukNodes.of_class("cache").each do |cache_hostname|
-      purge(base_path, cache_hostname)
+      purger.purge("http://#{cache_hostname}#{base_path}")
+    rescue Purger::PurgeFailed => e
+      raise VarnishCacheClearFailed, e
     end
   end
 
 private
 
-  attr_reader :logger
-
-  def purge(base_path, cache_hostname)
-    logger.info("Purging `#{base_path}` on `#{cache_hostname}`")
-
-    url = "http://#{cache_hostname}#{base_path}"
-
-    Net::HTTP.start(cache_hostname) do |http|
-      response = http.request(PurgeRequest.new(url))
-      status = response.code.to_i
-
-      unless (200...299).cover?(status)
-        raise VarnishCacheClearFailed.new("Received #{status} for `#{url}`")
-      end
-    end
-  end
-
-  class PurgeRequest < Net::HTTPRequest
-    METHOD = "PURGE".freeze
-    REQUEST_HAS_BODY = false
-    RESPONSE_HAS_BODY = true
-  end
+  attr_reader :logger, :purger
 
   class VarnishCacheClearFailed < StandardError; end
 end
